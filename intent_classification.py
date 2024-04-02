2617a47858fb68d578a8512c94c5dd6423102850
Homayoun S <hsohrab23@gmail.com>
2024-04-01 20:45:33 -0400

    intent_classification.py

--- intent_classification.py
+++ intent_classification.py
@@ -15,25 +15,16 @@ from transformers import AutoTokenizer, AutoModel
 import torch
 import json
 
+import sentence_embedder
+
 # Load the data from rag_data
 with codecs.open(r'RAG_data\rag_embedded_dataset.json', 'r', encoding='utf-8') as f:
     rag_data = json.load(f)
 
-def embed_sentence(sentence):
-    tokenizer_path = "C:/Projects/GPTChatbot_rag/outputFineTune"
-    model_path = "C:/Projects/GPTChatbot_rag/outputFineTune"
-    # Get embedding for a word
-    custom_tokenizer = GPT2Tokenizer.from_pretrained(tokenizer_path)
-    custom_model = GPT2Model.from_pretrained(model_path)
-    tokenized_input = custom_tokenizer(sentence, return_tensors="pt")
-    with torch.no_grad():
-        sentence_vec = custom_model(**tokenized_input)
-    return sentence_vec
-
 
 def predict_intent(input_sentence, rag_data=rag_data, distance_metric='euclidean'):
     input_sentence = sentence_normalizer.preprocess_main(input_sentence)
-    input_vec = embed_sentence(input_sentence).last_hidden_state.mean(dim=1).squeeze().numpy()
+    input_vec = sentence_embedder.embed_sentence(input_sentence).last_hidden_state.mean(dim=1).squeeze().numpy()
     
     # Extract patterns and corresponding tags
     X_train = []
